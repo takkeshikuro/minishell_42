dd1c3aa0599384f2afb374328cdba8d4c43336d2
Rayan MARECAR <rmarecar@paul-f3Br9s3.clusters.42paris.fr>
2023-06-14 13:39:49 +0200

    ajout fonction utils exec

--- src/exec/pipe_manage.c
+++ src/exec/pipe_manage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_manage.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marecarrayan <marecarrayan@student.42.f    +#+  +:+       +#+        */
+/*   By: rmarecar <rmarecar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:26:12 by marecarraya       #+#    #+#             */
-/*   Updated: 2023/06/06 16:46:38 by marecarraya      ###   ########.fr       */
+/*   Updated: 2023/06/14 13:39:11 by rmarecar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,20 +49,20 @@ void	pipe_init(t_main *data, int count)
 	if (count)
 	{
 		data->pipe_fd = malloc(sizeof(int) * count * 2);
-	    if (!data->pipe_fd)
-	    	return ;
-    	while (i < count)
-    	{
-    		if (pipe(data->pipe_fd + 2 * i) == -1)
-    			exit (1);
-    		i++;
-	    }
+		if (!data->pipe_fd)
+			return ;
+		while (i < count)
+		{
+			if (pipe(data->pipe_fd + 2 * i) == -1)
+				exit (1);
+			i++;
+		}
 	}
 	data->path = find_path(data->env_bis);
 	data->cmd_paths = ft_split(data->path, ':');
 }
 
-void    close_pipe(t_main *data, int count)
+void	close_pipe(t_main *data, int count)
 {
 	int	i;
 
@@ -73,7 +73,7 @@ void    close_pipe(t_main *data, int count)
 		i++;
 	}
 }
-/*
+
 void	pipe_redirect(t_main *data, int count, int pos)
 {	
 	if (pos == 0 && count)
@@ -88,7 +88,22 @@ void	pipe_redirect(t_main *data, int count, int pos)
 		dup2(data->pipe_fd[2 * pos + 1], 1);
 	}
 	close_pipe(data, count);
-}*/
+}
+
+int		contains_char(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*get_command(char **paths, char *cmd)
 {
 	char	*command;
@@ -96,13 +111,13 @@ char	*get_command(char **paths, char *cmd)
 
 	if (!paths || !cmd)
 		return (NULL);
-	/*
+	
 	if (contains_char(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
 			return (cmd);
 		return (NULL);
-	}*/
+	}
 	while (*paths)
 	{
 		tmp = ft_strjoin(*paths, "/");
@@ -129,13 +144,7 @@ void	child_process(t_main *data, int pos, int count, t_cmd_parse *node)
 	pid = fork();
 	if (pid == 0)
 	{
-		//pipe_redirect(data, count, pos);
-		if (pos == 0 && count)
-			dup2(data->pipe_fd[1], 1);
-		else if (pos == count && count)
-			dup2(data->pipe_fd[2 * pos - 2], 0);
-		else if (data->pipe_count && pos != 0 && data->pipe_count != pos)
-			duplicate2(data->pipe_fd[2 * pos - 2], data->pipe_fd[2 * pos + 1]);
+		pipe_redirect(data, count, pos);
 		close_pipe(data, count);
 		cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
 		//printf("\n%s\n", cmd);
@@ -151,6 +160,7 @@ void	wait_childs(int count)
 	i = 0;
 	while (i <= count)
 	{
+		write(2, "- ", 2);
 		waitpid(-1, NULL, 0);
 		i++;
 	}
@@ -176,4 +186,6 @@ void	execute_cmd(t_main *data)
 	}
 	close_pipe(data, count);
 	waitpid(-1, NULL, 0);
+	
+	//wait_childs(count);
 }
