0b361bf612b0eaec832c6446dd633d7bbd5f4c30
m1nhr <mrc.rayan@gmail.com>
2023-06-22 12:00:37 +0000

    pipeline redirection  done

--- src/exec/pipe_manage.c
+++ src/exec/pipe_manage.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_manage.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmorikaw <tmorikaw@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rmarecar <rmarecar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:26:12 by marecarraya       #+#    #+#             */
-/*   Updated: 2023/06/20 01:53:30 by tmorikaw         ###   ########.fr       */
+/*   Updated: 2023/06/14 13:39:11 by rmarecar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,23 +41,8 @@ int	lstsize(t_cmd_parse *lst)
 	return (size);
 }
 
-void	pipe_init(t_main *data, int count)
+void	pipe_init(t_main *data)
 {
-	int	i;
-
-	i = 0;
-	if (count)
-	{
-		data->pipe_fd = malloc(sizeof(int) * count * 2);
-		if (!data->pipe_fd)
-			return ;
-		while (i < count)
-		{
-			if (pipe(data->pipe_fd + 2 * i) == -1)
-				exit (1);
-			i++;
-		}
-	}
 	data->path = find_path(data->env_bis);
 	data->cmd_paths = ft_split(data->path, ':');
 }
@@ -72,22 +57,6 @@ void	close_pipe(t_main *data, int count)
 		close(data->pipe_fd[i]);
 		i++;
 	}
-}
-
-void	pipe_redirect(t_main *data, int count, int pos)
-{	
-	if (pos == 0 && count)
-		dup2(data->pipe_fd[1], 1);
-	else if (pos == count && count)
-	{
-	//	write(2, "\nok\n", 3);
-		dup2(data->pipe_fd[2 * pos - 2], 0);
-	}else if (count && pos && pos != count)
-	{
-		dup2(data->pipe_fd[2 * pos - 2], 0);
-		dup2(data->pipe_fd[2 * pos + 1], 1);
-	}
-	close_pipe(data, count);
 }
 
 int		contains_char(char *str, char c)
@@ -111,7 +80,7 @@ char	*get_command(char **paths, char *cmd)
 
 	if (!paths || !cmd)
 		return (NULL);
-
+	
 	if (contains_char(cmd, '/'))
 	{
 		if (access(cmd, X_OK) == 0)
@@ -131,28 +100,6 @@ char	*get_command(char **paths, char *cmd)
 	return (NULL);
 }
 
-void	duplicate2(int input, int output)
-{
-	dup2(input, 0);
-	dup2(output, 1);
-}
-
-void	child_process(t_main *data, int pos, int count, t_cmd_parse *node)
-{
-	int		pid;
-	char	*cmd;
-	pid = fork();
-	if (pid == 0)
-	{
-		pipe_redirect(data, count, pos);
-		close_pipe(data, count);
-		cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
-		//printf("\n%s\n", cmd);
-		execve(cmd, node->cmd_tab, data->env_bis);
-		exit(1);
-	}
-}
-
 void	wait_childs(int count)
 {
 	int	i;
@@ -166,26 +113,79 @@ void	wait_childs(int count)
 	}
 }
 
+void	pipe_work(t_main *data, int in, int out, t_cmd_parse *node)
+{
+	pid_t	pid;
+	char	*cmd;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (in)
+		{
+			dup2(in, 0);
+			close(in);
+		}
+		if (out != 1)
+		{
+			dup2(out, 1);
+			close(out);
+		}
+		cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
+		if (cmd == NULL)
+		{
+			write(2, node->cmd_tab[0], ft_strlen(node->cmd_tab[0]));
+			write(2, ": command not found\n", 20);
+			exit(1);
+		}
+		execve(cmd, node->cmd_tab, data->env_bis);
+		exit(1);
+	}
+}
+
 void	execute_cmd(t_main *data)
 {
-	int			count;
-	int			pos;
 	int			pid;
 	t_cmd_parse	*node;
+	int			in;
+	int			fd[2];
+	int			i;
+	char		*cmd;
 
 	node = data->cmd_parse;
-	count = lstsize(data->cmd_parse) - 1;
-	data->pipe_count = count;
-	pos = 0;
-	pipe_init(data, count);
-	while (pos <= count)
+	data->pipe_count = lstsize(data->cmd_parse) - 1;
+	i = 0;
+	in = 0;
+	pipe_init(data);
+	while (i < data->pipe_count)
 	{
-		child_process(data, pos, count, node);
+		pipe(fd);
+		pipe_work(data, in, fd[1], node);
+		close(fd[1]);
+		in = fd[0];
 		node = node->next;
-		pos++;
+		i++; 
 	}
-	close_pipe(data, count);
-	waitpid(-1, NULL, 0);
-	
-	//wait_childs(count);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (in)
+			dup2(in, 0);
+
+		cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
+		if (cmd == NULL)
+		{
+			write(2, node->cmd_tab[0], ft_strlen(node->cmd_tab[0]));
+			write(2, ": command not found\n", 20);
+			exit(1);
+		}
+		execve(cmd, node->cmd_tab, data->env_bis);
+		exit (1);
+	}
+	i = 0;
+	while (i <= data->pipe_count)
+	{
+		waitpid(-1, NULL, 0);
+		i++;
+	}
 }
