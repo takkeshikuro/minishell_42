f7a026c7bde1cb2fcbf79da8f201606b2a6f0e50
m1nhr <mrc.rayan@gmail.com>
2023-07-06 17:58:55 +0200

    gestion des variables d'environnement dans les heredoc

--- src/exec/pipe_manage.c
+++ src/exec/pipe_manage.c
@@ -95,23 +95,107 @@ int	child_processes(t_main *data, t_cmd_parse *node)
 	return (in);
 }
 
+int	has_variable(char *input)
+{
+	int	i;
+
+	i = 0;
+	while(input[i])
+	{
+		if (input[i] == '$')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_varname_len(char *str)
+{
+	int	len;
+	
+	len = 1;
+	while (str[len] > ' ' && str[len] < 127 && str[len] != '$')
+		len++;
+	return(len - 1);
+}
+
+char	*get_var_name(char *input)
+{
+	int		i;
+	int		len;
+	char	*name;
+
+	i = 1;
+	len = ft_varname_len(input);
+	name = malloc(sizeof(char) * (len + 1));
+	if (!name)
+	{
+		fprintf(stderr, "ERROR MALLOC : GET VAR NAME (here_doc manage)");
+		exit (1);
+	}
+	len = 0;
+	while (input[i] != '$' && input[i] > ' '  && input[i] < 127)
+	{
+		name[len] = input[i];
+		i++;
+		len++;
+	}
+	name[len] = 0;
+	return (name);
+}
+
+char	*get_var_content(t_main *data, char *var_name)
+{
+	int		i;
+	char	*content;
+	char	**envp;
+	int		len;
+
+	len = ft_strlen(var_name);
+	i = 0;
+	envp = data->env_bis;
+	while (envp[i])
+	{
+		if (!ft_strncmp(var_name, envp[i], len))
+			return (envp[i] + len + 1);
+		i++;
+	}
+	return (NULL);
+}
+
 void	here_doc_manage(t_main *data, t_cmd_parse *node, int fd[2])
 {
 	char	*input;
-	char	*here_doc;
 	int		size;
+	int		i;
+	char	*var_name;
+	char	*var_content;
 
-	here_doc = "";
 	while (1)
 	{
+		i = 0;
 		input = readline(">");
 		size = ft_strlen(input);
 		if (!ft_strncmp(input, node->redirection->str, size))
 			break ;
-		here_doc = ft_strjoin(here_doc, input);
-		here_doc = ft_strjoin(here_doc, "\n");
+		while (input[i])
+		{
+			if (input[i] == '$' && input[i + 1] != ' ')
+			{
+				var_name = get_var_name(input + i);
+				var_content = get_var_content(data, var_name);
+				write(fd[1], var_content, ft_strlen(var_content));
+				i += ft_strlen(var_name) + 1;
+				free(var_name);
+			}
+			else
+			{
+				write(fd[1], &input[i], 1);
+				i++;
+			}
+		}
+		write(fd[1], "\n", 1);
 	}
-	write(fd[1], here_doc, ft_strlen(here_doc));
 }
 
 void	last_process(t_main *data, t_cmd_parse *node, char *cmd, int in)
