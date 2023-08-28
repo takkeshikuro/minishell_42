287384e6869dbfb6a67ff7a25b26057ccee92308
Rayan MARECAR <rmarecar@made-f0Dr5s6.clusters.42paris.fr>
2023-08-28 18:11:19 +0200

    fix cat | ls

--- src/exec/pipe_manage.c
+++ src/exec/pipe_manage.c
@@ -6,59 +6,59 @@
 /*   By: rmarecar <rmarecar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:26:12 by marecarraya       #+#    #+#             */
-/*   Updated: 2023/08/25 16:24:06 by rmarecar         ###   ########.fr       */
+/*   Updated: 2023/08/28 18:08:50 by rmarecar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	pipe_work(t_main *data, int in, int out, t_cmd_parse *node, int fd0)
+void	pipe_work(t_main *data, int fd[2], t_cmd_parse *node, int old_fd[2])
 {
 	pid_t	pid;
 	char	*cmd;
-	int		fd[2];
 
 	pid = fork();
 	node->hd_check = 0;
 	cmd = NULL;
 	if (pid == 0)
 	{
-		redir_pipe(data, node, &in, &out);
-		if (in && node->hd_check == 0)
+		redir_pipe(data, node, &old_fd[0], &fd[1]);
+		if (old_fd[0] != -1 && node->hd_check == 0)
 		{
-			dup2(in, 0);
-			close(in);
+			dup2(old_fd[0], 0);
+			close(old_fd[0]);
 		}
-		if (out != 1)
+		if (fd[1] != 1)
 		{
-			dup2(out, 1);
-			close(out);
+			close(fd[0]);
+			dup2(fd[1], 1);
+			close(fd[1]);
 		}
 		builtin_exec(data, node);
 		cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
 		if (cmd == NULL)
 		{
-			if (in)
-				close(in);
-			close(fd0);
+			if (old_fd[0] != -1)
+				close(old_fd[0]);
+			close(fd[0]);
 		}
 		ft_execve(data, node, cmd);
 	}
-	if (in)
-		close(in);
+	if (old_fd[0] != -1)
+		close(old_fd[0]);
 }
 
-void	last_process(t_main *data, t_cmd_parse *node, char *cmd, int in)
+void	last_process(t_main *data, t_cmd_parse *node, char *cmd, int fd[2])
 {
 	int	out;
 	int	pid;
-	int	fd[2];
 
-	last_redir(data, node, &in, &out);
-	if (in && node->hd_check == 0)
+	last_redir(data, node, &fd[0], &fd[1]);
+	if (fd[0] && node->hd_check == 0)
 	{
-		dup2(in, 0);
-		close(in);
+		close(fd[1]);
+		dup2(fd[0], 0);
+		close(fd[0]);
 	}
 	builtin_exec(data, node);
 	cmd = get_command(data->cmd_paths, node->cmd_tab[0]);
@@ -71,32 +71,39 @@ void	last_process(t_main *data, t_cmd_parse *node, char *cmd, int in)
 void	exec(t_main *data, t_cmd_parse *node, char *cmd)
 {
 	int		i;
-	int		in;
 	int		pid;
 	int		fd[2];
-
+	int		old_fd[2];
+	
+	old_fd[0] = -1;
+	old_fd[1] = -1;
 	data->hd_pos = 0;
 	i = 0;
-	in = 0;
 	while (i < data->pipe_count)
 	{
 		pipe(fd);
-		pipe_work(data, in, fd[1], node, fd[0]);
-		close(fd[1]);
-		in = fd[0];
+		pipe_work(data, fd, node, old_fd);
+		if (old_fd[0] != -1)
+			close(old_fd[0]);
 		if (node->redirection)
 		{
 			if (node->redirection->operateur == LEFT_LEFT)
 				data->hd_pos++;
 		}
+		old_fd[0] = fd[0];
+		old_fd[1] = fd[1];
+		close(fd[1]);
 		node = node->next;
 		i++;
 	}
 	data->pid_last = fork();
 	if (data->pid_last == 0)
-		last_process(data, node, cmd, in);
+		last_process(data, node, cmd, fd);
 	if (data->pipe_count)
+	{
+		close(fd[1]);
 		close(fd[0]);
+	}
 }
 
 void	execute_cmd(t_main *data)
