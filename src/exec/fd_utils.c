a336448620bb69546ac8b3218232950684436e2b
Rayan MARECAR <rmarecar@MADE-f0Dr6s10.clusters.42paris.fr>
2023-08-30 17:00:47 +0200

    oubli suppr variable unused

--- src/exec/fd_utils.c
+++ src/exec/fd_utils.c
@@ -6,7 +6,7 @@
 /*   By: rmarecar <rmarecar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 15:26:12 by marecarraya       #+#    #+#             */
-/*   Updated: 2023/08/30 16:35:06 by rmarecar         ###   ########.fr       */
+/*   Updated: 2023/08/30 17:00:00 by rmarecar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	open_outfile(t_cmd_parse *node, int old_fd)
 {
 	int	out;
 
-	if (old_fd != 1 && old_fd)
+	if (old_fd != -1 && old_fd)
 		close(old_fd);
 	out = open(node->redirection->str, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (out == -1)
@@ -31,7 +31,7 @@ int	open_infile(t_cmd_parse *node, int old_fd)
 {
 	int	in;
 
-	if (old_fd)
+	if (old_fd != -1 && old_fd)
 		close(old_fd);
 	in = open(node->redirection->str, O_RDWR);
 	if (in == -1)
@@ -46,7 +46,7 @@ int	open_append(t_cmd_parse *node, int old_fd)
 {
 	int	out;
 
-	if (old_fd != 1 && old_fd)
+	if (old_fd != -1 && old_fd)
 		close(old_fd);
 	out = open(node->redirection->str, O_CREAT | O_WRONLY | O_APPEND, 0644);
 	if (out == -1)
